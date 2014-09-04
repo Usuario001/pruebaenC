bafc7b788cc341d19115625e19e504fc67252c8f
José <jose@MacBook-Pro-de-Jose.local>
2014-09-04 11:48:30 -0500

    Initial Commit

---
+++ pruebaenC/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  pruebaenC
+//
+//  Created by José on 04/09/14.
+//  Copyright (c) 2014 José. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
