7cafe52811365fad07f54f0d179c000514653432
Jose Manuel <patinogutierrez@icloud.com>
2014-11-11 17:08:04 -0600

    sumary
    descripcion

---
+++ pruebaenC/tama_enteros.c
@@ -0,0 +1,15 @@
+//
+//  tama_enteros.c
+//  pruebaenC
+//
+//  Created by José on 05/09/14.
+//  Copyright (c) 2014 José. All rights reserved.
+//
+#include <stdio.h>
+int main(int argc, const char * argv[])
+{
+    printf("El tamaño de short (entero corto), es: %lu \n", sizeof(short));
+    printf("El tamaño de int (entero), es: %lu \n", sizeof(int));
+    printf("El tamaño de long (entero largo), es: %lu \n", sizeof(long));
+    return 0;
+}
